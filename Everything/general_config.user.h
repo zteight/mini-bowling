86e7aa7c781883a1dc2909035f6770977bdbeb72
zt eight <eight@zt8.lol>
2026-08-24 22:13:18 -0500

    User configs

---
+++ Everything/general_config.user.h
@@ -0,0 +1,16 @@
+#define DECK_LED_LENGTH_L   12    // Left deck strip.  DEFAULT: 11
+#define DECK_LED_LENGTH_R   12    // Right deck strip. DEFAULT: 11
+
+#define DECK_LED_BRIGHTNESS    80 // Deck LED brightness (0-255). DEFAULT: 80
+
+
+#define SWEEP_GUARD_ANGLE   50    // Guard position.  DEFAULT: 50
+#define SWEEP_UP_ANGLE      85    // Up position.     DEFAULT: 85
+
+#define SWEEP_TWEEN_MS      500   // Tween duration.  DEFAULT: 500
+
+
+#define SCISSOR_GRAB_ANGLE  160   // DEFAULT: 140
+
+
+#define PAUSE_IDLE_MS           300000 // 5 minutes. DEFAULT: 300000
